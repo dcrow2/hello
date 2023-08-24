5b3601fa9ce7eaf9fbb7d44d10a4afa138afa159
Daniel Crow <dcrow2@huskers.unl.edu>
2023-08-24 09:40:50 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Daniel Crow
+ *         dcrow2@huskers.unl.edu
+ * Date: 2023/08/24
+ *
+ * Prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Daniel Crow\n");
+  
+  return 0;
+}
